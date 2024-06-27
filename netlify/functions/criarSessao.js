f23793243e713066145f1651abcd3e81774b8311
erissonmcc <erissonmiqueas@gmail.com>
2024-06-27 19:48:10 -0300

    Tentando corrijir o poblema do cors ;)

--- netlify/functions/criarSessao.js
+++ netlify/functions/criarSessao.js
@@ -1,12 +1,18 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const cors = require('cors');
+const cors = require('cors')();
 
 exports.handler = async (event, context) => {
   console.log('Nova solicitação recebida:', event.httpMethod, event.path);
 
-// Aplicar CORS
+  // Aplicar CORS
   cors(event, context, () => {});
-  
+
+  // Configuração dos cabeçalhos padrão
+  const headers = {
+    'Access-Control-Allow-Origin': '*', // Permitir todas as origens. Modifique conforme necessário.
+    'Access-Control-Allow-Headers': 'Content-Type',
+  };
+
   // Verificar o método da solicitação
   if (event.httpMethod === 'OPTIONS') {
     // Responder a solicitação OPTIONS sem processar a função
@@ -27,50 +33,51 @@ exports.handler = async (event, context) => {
       body: JSON.stringify({ error: 'Método não permitido' }),
     };
   }
-      // Obter dados do corpo da solicitação (dados do usuário)
-      const requestBody = JSON.parse(event.body);
-      const { uid, email, displayName } = requestBody;
 
-      console.log('Dados do usuário:', { uid, email, displayName });
+  // Obter dados do corpo da solicitação (dados do usuário)
+  const requestBody = JSON.parse(event.body);
+  const { uid, email, displayName } = requestBody;
 
-      // Criar sessão de checkout na Stripe
-      stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        line_items: [{
-          price_data: {
-            currency: 'brl',
-            product_data: {
-              name: 'Postiça realista iniciante e aperfeiçoamento',
-            },
-            unit_amount: 3400, // Valor em centavos (R$ 34,00)
+  console.log('Dados do usuário:', { uid, email, displayName });
+
+  // Criar sessão de checkout na Stripe
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [{
+        price_data: {
+          currency: 'brl',
+          product_data: {
+            name: 'Postiça realista iniciante e aperfeiçoamento',
           },
-          quantity: 1,
-        }],
-        mode: 'payment',
-        success_url: 'http://localhost:2435/storage/emulated/0/gessica/public/index.html',
-        cancel_url: 'http://localhost:2435/storage/emulated/0/gessica/public/index.html',
-        customer_email: email,
-        billing_address_collection: 'required',
-        metadata: {
-          uid: uid,
-          displayName: displayName,
+          unit_amount: 3400, // Valor em centavos (R$ 34,00)
         },
-      }).then(session => {
-        console.log('Sessão criada com sucesso:', session);
-        // Retornar ID da sessão criada
-        resolve({
-          statusCode: 200,
-          headers,
-          body: JSON.stringify({ id: session.id }),
-        });
-      }).catch(error => {
-        console.error('Erro ao criar sessão de checkout:', error);
-        reject({
-          statusCode: 500,
-          headers,
-          body: JSON.stringify({ error: 'Erro ao criar sessão de checkout' }),
-        });
-      });
+        quantity: 1,
+      }],
+      mode: 'payment',
+      success_url: 'http://localhost:2435/storage/emulated/0/gessica/public/index.html',
+      cancel_url: 'http://localhost:2435/storage/emulated/0/gessica/public/index.html',
+      customer_email: email,
+      billing_address_collection: 'required',
+      metadata: {
+        uid: uid,
+        displayName: displayName,
+      },
     });
-  });
+
+    console.log('Sessão criada com sucesso:', session);
+    // Retornar ID da sessão criada
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify({ id: session.id }),
+    };
+  } catch (error) {
+    console.error('Erro ao criar sessão de checkout:', error);
+    return {
+      statusCode: 500,
+      headers,
+      body: JSON.stringify({ error: 'Erro ao criar sessão de checkout' }),
+    };
+  }
 };
