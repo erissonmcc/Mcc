5bdd3e83d2bcc1afdb009433f4969829bf943950
erissonmcc <erissonmiqueas@gmail.com>
2024-06-27 20:25:08 -0300

    Tentando corrijir erro de crash

--- netlify/functions/criarSessao.js
+++ netlify/functions/criarSessao.js
@@ -1,96 +1,79 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const cors = require('cors');
 
 exports.handler = async (event, context) => {
   console.log('Nova solicitação recebida:', event.httpMethod, event.path);
 
-  // Aplicar CORS
-  const corsHandler = cors({
-    origin: '*', // Permitir todas as origens. Modifique conforme necessário.
-    allowedHeaders: 'Content-Type',
-  });
+  // Configuração dos cabeçalhos padrão
+  const headers = {
+    'Access-Control-Allow-Origin': '*', // Permitir todas as origens. Modifique conforme necessário.
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'POST, OPTIONS',
+  };
 
-  return new Promise((resolve, reject) => {
-    corsHandler(event, context, (err) => {
-      if (err) {
-        console.error('Erro ao aplicar CORS:', err);
-        reject(err);
-        return;
-      }
+  // Verificar o método da solicitação
+  if (event.httpMethod === 'OPTIONS') {
+    // Responder a solicitação OPTIONS sem processar a função
+    console.log('Solicitação OPTIONS recebida');
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify({ message: 'OPTIONS recebido' }),
+    };
+  }
 
-      // Configuração dos cabeçalhos padrão
-      const headers = {
-        'Access-Control-Allow-Origin': '*', // Permitir todas as origens. Modifique conforme necessário.
-        'Access-Control-Allow-Headers': 'Content-Type',
-      };
+  if (event.httpMethod !== 'POST') {
+    console.log('Solicitação não permitida:', event.httpMethod);
+    return {
+      statusCode: 405,
+      headers,
+      body: JSON.stringify({ error: 'Método não permitido' }),
+    };
+  }
 
-      // Verificar o método da solicitação
-      if (event.httpMethod === 'OPTIONS') {
-        // Responder a solicitação OPTIONS sem processar a função
-        console.log('Solicitação OPTIONS recebida');
-        resolve({
-          statusCode: 200,
-          headers,
-          body: JSON.stringify({ message: 'OPTIONS recebido' }),
-        });
-        return;
-      }
+  try {
+    // Obter dados do corpo da solicitação (dados do usuário)
+    const requestBody = JSON.parse(event.body);
+    const { uid, email, displayName } = requestBody;
 
-      // Verificar o método da solicitação
-      if (event.httpMethod !== 'POST') {
-        console.log('Solicitação não permitida:', event.httpMethod);
-        resolve({
-          statusCode: 405,
-          headers,
-          body: JSON.stringify({ error: 'Método não permitido' }),
-        });
-        return;
-      }
+    console.log('Dados do usuário:', { uid, email, displayName });
 
-      // Obter dados do corpo da solicitação (dados do usuário)
-      const requestBody = JSON.parse(event.body);
-      const { uid, email, displayName } = requestBody;
-
-      console.log('Dados do usuário:', { uid, email, displayName });
-
-      // Criar sessão de checkout na Stripe
-      stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        line_items: [{
-          price_data: {
-            currency: 'brl',
-            product_data: {
-              name: 'Postiça realista iniciante e aperfeiçoamento',
-            },
-            unit_amount: 3400, // Valor em centavos (R$ 34,00)
+    // Criar sessão de checkout na Stripe
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [{
+        price_data: {
+          currency: 'brl',
+          product_data: {
+            name: 'Postiça realista iniciante e aperfeiçoamento',
           },
-          quantity: 1,
-        }],
-        mode: 'payment',
-        success_url: 'http://localhost:2435/storage/emulated/0/gessica/public/index.html',
-        cancel_url: 'http://localhost:2435/storage/emulated/0/gessica/public/index.html',
-        customer_email: email,
-        billing_address_collection: 'required',
-        metadata: {
-          uid: uid,
-          displayName: displayName,
+          unit_amount: 3400, // Valor em centavos (R$ 34,00)
         },
-      }).then(session => {
-        console.log('Sessão criada com sucesso:', session);
-        // Retornar ID da sessão criada
-        resolve({
-          statusCode: 200,
-          headers,
-          body: JSON.stringify({ id: session.id }),
-        });
-      }).catch(error => {
-        console.error('Erro ao criar sessão de checkout:', error);
-        reject({
-          statusCode: 500,
-          headers,
-          body: JSON.stringify({ error: 'Erro ao criar sessão de checkout' }),
-        });
-      });
+        quantity: 1,
+      }],
+      mode: 'payment',
+      success_url: 'http://localhost:2435/storage/emulated/0/gessica/public/index.html',
+      cancel_url: 'http://localhost:2435/storage/emulated/0/gessica/public/index.html',
+      customer_email: email,
+      billing_address_collection: 'required',
+      metadata: {
+        uid: uid,
+        displayName: displayName,
+      },
     });
-  });
+
+    console.log('Sessão criada com sucesso:', session);
+    // Retornar ID da sessão criada
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify({ id: session.id }),
+    };
+  } catch (error) {
+    console.error('Erro ao criar sessão de checkout:', error);
+    return {
+      statusCode: 500,
+      headers,
+      body: JSON.stringify({ error: 'Erro ao criar sessão de checkout' }),
+    };
+  }
 };
