cf41d22006f4438eedc91e8c6dcc087d1537c487
erissonmcc <erissonmiqueas@gmail.com>
2024-06-28 18:01:01 -0300

    Correção de bugs

--- netlify/functions/createSession.js
+++ netlify/functions/createSession.js
@@ -1,5 +1,5 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const { db, auth } = require('./firebaseAdmin'); // Certifique-se de que o Firebase Admin SDK está configurado
+const { db, auth, admin } = require('./firebaseAdmin'); // Certifique-se de que o Firebase Admin SDK está configurado
 
 exports.handler = async (event, context) => {
   console.log('Nova solicitação recebida:', event.httpMethod, event.path);
