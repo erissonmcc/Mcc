ff057a5b6228b62abc2964fa0373b14a5badf6c1
erissonmcc <erissonmiqueas@gmail.com>
2024-06-28 11:46:21 -0300

    Removendo pix como forma de pagamento (Não suportado na stripe.

--- netlify/functions/createSession.js
+++ netlify/functions/createSession.js
@@ -56,7 +56,7 @@ exports.handler = async (event, context) => {
 
     // Criar sessão de checkout na Stripe
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card', 'boleto', 'pix'],
+      payment_method_types: ['card', 'boleto'],
       line_items: [{
         price_data: {
           currency: 'brl',
