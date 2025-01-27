50212ce9a1513936d459e19421b08c47a12643dc
erissonmcc <erissonmiqueas@gmail.com>
2025-01-27 00:06:41 -0300

    Correção do webhook

--- api/webhook.js
+++ api/webhook.js
@@ -1,6 +1,5 @@
 import stripePackage from 'stripe';
 import nodemailer from 'nodemailer';
-import fetch from 'node-fetch';
 import admin from 'firebase-admin';
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
