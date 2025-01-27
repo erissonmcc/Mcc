6d23f13ae7657e3022569da034e46a02dee7064c
erissonmcc <erissonmiqueas@gmail.com>
2025-01-26 23:58:47 -0300

    .

--- api/webhook.js
+++ api/webhook.js
@@ -14,6 +14,7 @@ if (!admin.apps.length) {
 const stripe = stripePackage(process.env.STRIPE_SECRET_KEY);
 
 const db = admin.firestore();
+const auth = admin.auth();
 
 
 export default async function handler(req, res) {
